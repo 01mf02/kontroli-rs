e65f1564d157f0093e4a1e188d7fa5b76621d084
Michael Färber <01mf02@gmail.com>
2019-12-11 10:17:14 +0100

    Start work on typechecking.

--- src/main.rs
+++ src/main.rs
@@ -27,6 +27,7 @@ type DeBruijn = usize;
 
 #[derive(Debug, PartialEq, Clone)]
 pub enum Term {
+  Kind,
   Type,
   Symb(String),
   BVar(DeBruijn),
@@ -250,9 +251,9 @@ use super::*;
 pub type SymTable = FnvHashMap<String, ()>;
 type Bound = Vec<String>;
 
-fn bind<A, F>(bnd: &mut Bound, arg: Option<String>, f: F) -> A
+pub fn bind<X, A, F>(bnd: &mut Vec<X>, arg: Option<X>, f: F) -> A
 where
-  F: FnOnce(&mut Bound) -> A
+  F: FnOnce(&mut Vec<X>) -> A
 {
   match arg {
     Some(id) => {
@@ -268,6 +269,7 @@ where
 fn term(sym: &SymTable, bnd: &mut Bound, tm: Term) -> Term {
   use super::Term::*;
   match tm {
+    Kind => Kind,
     Type => Type,
     Symb(s) => {
       match bnd.iter().rev().position(|id| *id == s) {
@@ -300,6 +302,97 @@ pub fn dcommand(sym: &SymTable, bnd: &mut Bound, dcmd: DCommand) -> DCommand {
   dcmd
     .map_type(|tm| Box::new(term(sym, bnd, *tm)))
     .map_term(|tm| Box::new(term(sym, bnd, *tm)))
+}
+
+}
+
+
+mod reduce {
+
+use super::*;
+
+// symbol -> type
+pub type Signature = FnvHashMap<String, Term>;
+
+pub fn whnf(sig: &Signature, tm: Term) -> Term {
+  tm
+}
+
+pub fn convertible(sig: &Signature, tm1: Term, tm2: Term) -> bool {
+  true
+}
+
+}
+
+
+mod typing {
+
+use super::*;
+use super::Term::*;
+use super::reduce::Signature;
+
+// DB -> type
+type Context = Vec<Term>;
+
+fn bind<F, A>(sig: &Signature, ctx: &mut Context, ty: Term, f: F) -> A
+where
+  F: FnOnce(&mut Context) -> A
+{
+  match infer(sig, ctx, ty.clone()) {
+    Type => scope::bind(ctx, Some(ty), |ctx| f(ctx)),
+    _ => panic!("convertibility error")
+  }
+}
+
+fn infer(sig: &Signature, ctx: &mut Context, tm: Term) -> Term {
+  match tm {
+    Kind => panic!("kind is not typable"),
+    Type => Kind,
+    Symb(s) => sig.get(&s).unwrap().clone(),
+    BVar(x) => ctx.iter().rev().nth(x).unwrap().clone(),
+    Abst((id, Some(ty)), tm) => {
+      match bind(sig, ctx, *ty.clone(), |ctx| infer(sig, ctx, *tm)) {
+        Kind => panic!("unexpected kind"),
+        tm_ty => Prod((id, Some(ty)), Box::new(tm_ty))
+      }
+    },
+    Prod((id, Some(ty)), tm) => {
+      match bind(sig, ctx, *ty.clone(), |ctx| infer(sig, ctx, *tm)) {
+        tm_ty @ Kind | tm_ty @ Type => tm_ty,
+        _ => panic!("sort expected")
+      }
+    },
+    Abst((_, None), _) | Prod((_, None), _) => panic!("domain-free lambda/pi"),
+    _ => panic!("todo")
+  }
+}
+
+fn check(sig: &Signature, ctx: &mut Context, tm: Term, ty_exp: Term) {
+  match tm {
+    Abst((id, None), tm) => {
+      match reduce::whnf(sig, ty_exp) {
+        Prod((_, Some(ty_a)), ty_b) =>
+          scope::bind(ctx, Some(*ty_a), |ctx| check(sig, ctx, *tm, *ty_b)),
+        _ => panic!("product expected")
+      }
+    },
+    Abst ((id, Some(ty_a_exp)), tm) => {
+      match reduce::whnf(sig, ty_exp) {
+        Prod((_, Some(ty_a)), ty_b) => {
+          let _ = infer(sig, ctx, *ty_a.clone());
+          if ! reduce::convertible(sig, *ty_a_exp.clone(), *ty_a) {
+            panic!("convertibility error")
+          }
+          else {
+            // TODO: can we use ty_a instead of ty_a_exp here?
+            scope::bind(ctx, Some(*ty_a_exp), |ctx| check(sig, ctx, *tm, *ty_b))
+          }
+        },
+        _ => panic!("product expected")
+      }
+    }
+    _ => panic!("todo")
+  }
 }
 
 }
