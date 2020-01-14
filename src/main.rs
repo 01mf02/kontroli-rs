756a49274cd4ca4f8d2a385e38a5b85a237af303
Michael Färber <michael.faerber@gedenkt.at>
2020-01-14 15:04:02 +0100

    Handle theorems.

--- src/main.rs
+++ src/main.rs
@@ -76,7 +76,8 @@ fn run(filename: &str) -> Result<(), CliError> {
                     (oty, Some(tm)) => signature::Entry::define(&sig, false, oty, *tm),
                     (None, None) => panic!("both type and term are empty"),
                 },
-                _ => unimplemented!(),
+                DCommand::Theorem(ty, tm) =>
+                    signature::Entry::define(&sig, true, Some(ty), *tm),
             }?;
             if sig.insert(id, entry).is_some() {
                 panic!("symbol redeclaration");
