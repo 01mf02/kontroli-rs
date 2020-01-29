be1f1d68549a7a9c5f05676318588492222f00f7
Michael Färber <michael.faerber@gedenkt.at>
2020-01-29 11:36:16 +0100

    Small simplification.

--- src/rule.rs
+++ src/rule.rs
@@ -30,16 +30,16 @@ impl From<Term> for Pattern {
         use Term::*;
         match tm {
             Appl(head, mut args) => match *head {
-                Symb(s) => Pattern::Symb(s, args.into_iter().map(Self::from).collect()),
+                Symb(s) => Self::Symb(s, args.into_iter().map(Self::from).collect()),
                 Appl(head2, mut args2) => {
                     args2.append(&mut args);
                     Self::from(Appl(head2, args2))
                 }
                 _ => unimplemented!(),
             },
-            Symb(s) => Pattern::Symb(s, Vec::new()),
+            Symb(s) => Self::Symb(s, Vec::new()),
             // TODO: warn if arg.type given?
-            Abst(arg, tm) => Pattern::Abst(arg.id, Box::new(Self::from(*tm))),
+            Abst(arg, tm) => Self::Abst(arg.id, Box::new(Self::from(*tm))),
             _ => unimplemented!(),
         }
     }
