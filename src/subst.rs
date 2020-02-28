c4341cc4d6cb05c92a0de01446a3de49205449f5
Michael Färber <michael.faerber@gedenkt.at>
2020-02-28 10:58:34 +0100

    Create new RTerm only if changed by substitution.

--- src/subst.rs
+++ src/subst.rs
@@ -1,13 +1,34 @@
 use crate::term::{Arg, RTerm, Term};
 
 impl Arg {
-    pub fn apply_subst<S>(&self, subst: &S, k: usize) -> Self
+    /// Return the substitution of an argument only if it changed.
+    fn subst<S>(&self, subst: &S, k: usize) -> Option<Self>
     where
         S: Fn(usize, usize) -> RTerm,
     {
-        Self {
-            id: self.id.clone(),
-            ty: self.ty.as_ref().map(|a| a.clone().apply_subst(subst, k)),
+        let ty = self.ty.as_ref()?;
+        let ty2 = ty.clone().apply_subst(subst, k);
+        if ty.ptr_eq(&ty2) {
+            None
+        } else {
+            Some(Self {
+                id: self.id.clone(),
+                ty: Some(ty2),
+            })
+        }
+    }
+
+    /// Return the substitution of an argument and a term
+    /// only if at least one them changed.
+    fn subst_bound<S>(&self, f: &RTerm, subst: &S, k: usize) -> Option<(Self, RTerm)>
+    where
+        S: Fn(usize, usize) -> RTerm,
+    {
+        let f2 = f.clone().apply_subst(subst, k + 1);
+        if f.ptr_eq(&f2) {
+            Some((self.subst(subst, k)?, f2))
+        } else {
+            Some((self.subst(subst, k).unwrap_or_else(|| self.clone()), f2))
         }
     }
 }
@@ -17,25 +38,28 @@ impl RTerm {
     where
         S: Fn(usize, usize) -> RTerm,
     {
-        // TODO: only create new RTerm if something has changed
         match &*self {
             Term::BVar(n) if *n >= k => subst(*n, k),
             Term::Appl(f, args) => {
                 let f2 = f.clone().apply_subst(subst, k);
-                let args2 = args
+                let args2: Vec<RTerm> = args
                     .iter()
                     .map(|a| a.clone().apply_subst(subst, k))
                     .collect();
-                RTerm::new(Term::Appl(f2, args2))
+                if f.ptr_eq(&f2) && args.iter().zip(args2.iter()).all(|(a, a2)| a.ptr_eq(a2)) {
+                    self
+                } else {
+                    Self::new(Term::Appl(f2, args2))
+                }
             }
-            Term::Abst(arg, f) => {
-                let f2 = f.clone().apply_subst(subst, k + 1);
-                RTerm::new(Term::Abst(arg.apply_subst(subst, k), f2))
-            }
-            Term::Prod(arg, f) => {
-                let f2 = f.clone().apply_subst(subst, k + 1);
-                RTerm::new(Term::Prod(arg.apply_subst(subst, k), f2))
-            }
+            Term::Abst(arg, f) => match arg.subst_bound(f, subst, k) {
+                Some((arg2, f2)) => Self::new(Term::Abst(arg2, f2)),
+                None => self,
+            },
+            Term::Prod(arg, f) => match arg.subst_bound(f, subst, k) {
+                Some((arg2, f2)) => Self::new(Term::Prod(arg2, f2)),
+                None => self,
+            },
             _ => self,
         }
     }
