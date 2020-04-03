12c18e397d93ff489e8c073885a2b5d583a5889a
Michael Färber <01mf02@gmail.com>
2020-04-03 10:29:46 +0200

    Document patterns.

--- src/pattern.rs
+++ src/pattern.rs
@@ -1,4 +1,4 @@
-//! Rewrite patterns.
+//! Shared rewrite patterns.
 
 use crate::scope::Error;
 use crate::symbol::Symbol;
@@ -6,6 +6,23 @@ use crate::term::{fmt_appl, Term};
 use alloc::vec::Vec;
 use core::{convert::TryFrom, fmt};
 
+/// Miller variable.
+///
+/// This refers to the variables appearing in a rewrite pattern
+/// bound by the rewrite rule's context.
+/// Historically, this type was created to distinguish variables bound
+/// by the rule context and
+/// by lambda abstraction, as occurring in higher-order patterns.
+/// Because Kontroli abandoned higher-order patterns,
+/// this distinction is no longer necessary; however, the name remains as
+/// a tribute to Miller's contributions to higher-order rewriting.
+///
+/// Reference:
+/// Dale Miller:
+/// A Logic Programming Language with Lambda-Abstraction,
+/// Function Variables, and Simple Unification.
+/// J. Log. Comput. 1(4): 497-536 (1991).
+/// doi: [10.1093/logcom/1.4.497](https://doi.org/10.1093/logcom/1.4.497)
 #[derive(Copy, Clone, PartialEq, Eq, Hash)]
 pub struct Miller(pub usize);
 
@@ -15,13 +32,24 @@ impl fmt::Display for Miller {
     }
 }
 
+/// Shared rewrite pattern.
+///
+/// This may be nonlinear; e.g. `eq X X` is a valid pattern.
 #[derive(Clone)]
 pub enum Pattern {
-    MVar(Miller),
+    /// matches an application
     Symb(Symbol, Vec<Pattern>),
+    /// matches any term, variable may appear multiple times in
+    /// both left-hand and right-hand sides of rewrite rule
+    MVar(Miller),
+    /// matches any term
     Joker,
 }
 
+/// Pattern at the left-hand side of a rewrite rule.
+///
+/// The top pattern of a rule must be an application of patterns to a symbol.
+/// This is to exclude rules matching any term, such as `[X] X --> f`.
 #[derive(Clone)]
 pub struct TopPattern {
     pub symbol: Symbol,
