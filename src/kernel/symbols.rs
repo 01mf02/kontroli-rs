31a72f45990bf6cceadc64ee07d9035605f2c4b4
Michael Färber <michael.faerber@gedenkt.at>
2020-06-06 07:43:13 +0200

    Use HashSet for symbols table.

--- src/kernel/symbols.rs
+++ src/kernel/symbols.rs
@@ -2,37 +2,32 @@
 
 use super::Symbol;
 use crate::error::SymbolsError as Error;
-use alloc::string::String;
 use core::iter::FromIterator;
-use fnv::FnvHashMap;
+use fnv::FnvHashSet;
 
 #[derive(Default)]
-pub struct Symbols<'s>(FnvHashMap<String, Symbol<'s>>);
+pub struct Symbols<'s>(FnvHashSet<&'s str>);
 
 impl<'s> Symbols<'s> {
     pub fn new() -> Self {
         Default::default()
     }
 
-    pub fn get(&self, s: &str) -> Option<&Symbol<'s>> {
-        self.0.get(s)
+    pub fn get(&self, s: &str) -> Option<Symbol<'s>> {
+        self.0.get(s).copied().map(Symbol::new)
     }
 
     pub fn insert(&mut self, s: &'s str) -> Result<Symbol<'s>, Error> {
-        let sym = Symbol::new(s);
-        if self.0.insert(String::from(s), sym).is_some() {
+        // `insert` returns false if the symbol is already in the set
+        if !self.0.insert(s) {
             return Err(Error::Reinsertion);
         }
-        Ok(sym)
+        Ok(Symbol::new(s))
     }
 }
 
 impl<'s> FromIterator<&'s str> for Symbols<'s> {
     fn from_iter<I: IntoIterator<Item = &'s str>>(iter: I) -> Self {
-        Self(
-            iter.into_iter()
-                .map(|s| (String::from(s), Symbol::new(s)))
-                .collect(),
-        )
+        Self(iter.into_iter().collect())
     }
 }
