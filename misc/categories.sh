d36f4e4f966d1248362765bc11e8dfc4195f8b66
Michael Färber <01mf02@gmail.com>
2020-07-09 10:45:19 +0200

    Add script to generate pie chart statistics for kernel.

---
+++ misc/categories.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# Generate kernel size statistics to be used with pgf-pie.
+# Call from main directory.
+
+cd src/kernel
+tokei -f * -o json | \
+  jq '.inner.Rust.stats | map({code, name})' | \
+  sed \
+    -e "s/\(reduce\|matching\|state\).rs/Reduction/" \
+    -e "s/typing.rs/Typing/" \
+    -e "s/subst.rs/Substitution/" \
+    -e "s/share.rs/Sharing/" \
+    -e "s/rterm.rs/Terms/" \
+    -e "s/convertible.rs/Convertibility/" \
+    -e "s/signature.rs/Signature/" \
+    -e "s/mod.rs/API/" | \
+  jq 'group_by(.name) | map({name: .[0].name, code: map(.code) | add})' | \
+  jq 'sort_by(.code) | reverse' | \
+  jq -j '.[] | .code, "/", .name, ",\n"'
