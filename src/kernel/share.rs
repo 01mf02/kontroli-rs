be706481475e79648d7fe6652f4e5e1264d85add
Michael Färber <01mf02@gmail.com>
2020-07-20 12:15:41 +0200

    Format.

--- src/kernel/share.rs
+++ src/kernel/share.rs
@@ -1,14 +1,13 @@
 //! Convert from scoped to shared structures.
 
 use super::{Intro, RTerm, Rc, Rule, Term};
-use crate::scope::{Intro as SIntro, Term as STerm, RTerm as SRTerm, Rule as SRule};
+use crate::scope::{Intro as SIntro, RTerm as SRTerm, Rule as SRule, Term as STerm};
 
 impl<'s> From<STerm<'s>> for Term<'s> {
     fn from(tm: STerm<'s>) -> Self {
         tm.map(Rc::new, RTerm::from)
     }
 }
-
 
 impl<'s> From<SRTerm<'s>> for RTerm<'s> {
     fn from(tm: SRTerm<'s>) -> Self {
