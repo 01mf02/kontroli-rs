d7ae159a0a788cf336941bae5d9dd15c8d9bd542
Michael Färber <01mf02@gmail.com>
2020-07-24 15:23:13 +0200

    Consider KOFLAGS.

--- examples/kontroli.mk
+++ examples/kontroli.mk
@@ -2,4 +2,4 @@
 	@echo $<
 
 %.koo:
-	$(MAKE) --silent -f deps.mk -f kontroli.mk $*.dko | xargs cat | kocheck -
+	$(MAKE) --silent -f deps.mk -f kontroli.mk $*.dko | xargs cat | kocheck $(KOFLAGS) -
