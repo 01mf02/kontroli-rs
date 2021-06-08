8482fd5c92ad06f9d501fcc7ba9eba48b9f407f1
Michael Färber <01mf02@gmail.com>
2021-06-08 16:27:27 +0200

    Eliminate jokers and minimise number of context variables.

--- kontroli/src/pattern.rs
+++ kontroli/src/pattern.rs
@@ -22,38 +22,65 @@ use core::fmt::{self, Display};
 /// Function Variables, and Simple Unification.
 /// J. Log. Comput. 1(4): 497-536 (1991).
 /// doi: [10.1093/logcom/1.4.497](https://doi.org/10.1093/logcom/1.4.497)
-pub type Miller = usize;
+#[derive(Copy, Clone, Debug)]
+pub struct Miller(usize);
+
+impl From<usize> for Miller {
+    fn from(u: usize) -> Self {
+        Self(u)
+    }
+}
+
+impl From<Miller> for usize {
+    fn from(m: Miller) -> Self {
+        m.0
+    }
+}
+
+impl Display for Miller {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "μ{}", self.0)
+    }
+}
 
 /// Rewrite pattern.
 ///
 /// This may be nonlinear; e.g. `eq X X` is a valid pattern.
 #[derive(Clone)]
-pub enum Pattern<S> {
+pub enum Pattern<S, V> {
     /// matches an application
-    Symb(S, Vec<Pattern<S>>),
+    Symb(S, Vec<Pattern<S, V>>),
     /// matches any term, variable may appear multiple times in
     /// both left-hand and right-hand sides of rewrite rule
-    MVar(Miller),
-    /// matches any term
-    Joker,
+    Var(V),
 }
 
 /// Pattern at the left-hand side of a rewrite rule.
 ///
 /// The top pattern of a rule must be an application of patterns to a symbol.
 /// This is to exclude rules matching any term, such as `[X] X --> f`.
-pub type TopPattern<S> = crate::Application<S, Pattern<S>>;
+pub type TopPattern<S, V> = crate::Application<S, Pattern<S, V>>;
 
-impl<S> From<TopPattern<S>> for Pattern<S> {
-    fn from(tp: TopPattern<S>) -> Self {
+impl<S, V> Pattern<S, V> {
+    pub fn map_vars<W>(self, f: &mut impl FnMut(V) -> W) -> Pattern<S, W> {
+        use Pattern::*;
+        match self {
+            Symb(s, args) => Symb(s, args.into_iter().map(|a| a.map_vars(f)).collect()),
+            Var(v) => Var(f(v)),
+        }
+    }
+}
+
+impl<S, V> From<TopPattern<S, V>> for Pattern<S, V> {
+    fn from(tp: TopPattern<S, V>) -> Self {
         Self::Symb(tp.symbol, tp.args)
     }
 }
 
-impl<S> TryFrom<Pattern<S>> for TopPattern<S> {
+impl<S, V> TryFrom<Pattern<S, V>> for TopPattern<S, V> {
     type Error = ();
 
-    fn try_from(p: Pattern<S>) -> Result<Self, Self::Error> {
+    fn try_from(p: Pattern<S, V>) -> Result<Self, Self::Error> {
         match p {
             Pattern::Symb(symbol, args) => Ok(TopPattern { symbol, args }),
             _ => Err(()),
@@ -61,12 +88,11 @@ impl<S> TryFrom<Pattern<S>> for TopPattern<S> {
     }
 }
 
-impl<S: Display> Display for Pattern<S> {
+impl<S: Display, V: Display> Display for Pattern<S, V> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             Self::Symb(s, pats) => fmt_appl(s, pats, f),
-            Self::MVar(m) => write!(f, "μ{}", m),
-            Self::Joker => write!(f, "_"),
+            Self::Var(m) => m.fmt(f),
         }
     }
 }
--- kontroli/src/scope/pattern.rs
+++ kontroli/src/scope/pattern.rs
@@ -4,7 +4,7 @@ use super::Symbol;
 pub use crate::pattern::Miller;
 
 /// Rewrite pattern.
-pub type Pattern<'s> = crate::Pattern<Symbol<'s>>;
+pub type Pattern<'s> = crate::Pattern<Symbol<'s>, Miller>;
 
 /// Pattern at the left-hand side of a rewrite rule.
-pub type TopPattern<'s> = crate::pattern::TopPattern<Symbol<'s>>;
+pub type TopPattern<'s> = crate::pattern::TopPattern<Symbol<'s>, Miller>;
