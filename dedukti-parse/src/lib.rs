a466bb701f0f41339a59d978f3b9d0d12792ba0f
Michael Färber <michael.faerber@gedenkt.at>
2021-07-01 08:07:53 +0200

    Provide lexing helper functions.

--- dedukti-parse/src/lib.rs
+++ dedukti-parse/src/lib.rs
@@ -6,4 +6,17 @@ pub mod lex;
 pub mod parse;
 
 pub use lex::Token;
-pub use parse::{Command, Intro, Rule, Term};
+pub use parse::{Command, Error, Intro, Rule, Term};
+
+use alloc::vec::Vec;
+use logos::Logos;
+
+pub fn lex<'s>(s: &'s str) -> impl Iterator<Item = Token<'s>> {
+    Token::lexer(s).filter(|token| *token != Token::Space)
+}
+
+#[cfg(feature = "itertools")]
+pub fn lexes<'s>(s: &'s str) -> impl Iterator<Item = Result<Vec<Token<'s>>, Error>> {
+    use itertools::Itertools;
+    Token::lexer(s).batching(parse::yield_command)
+}
