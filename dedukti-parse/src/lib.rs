479f0e324c1ab4c26dbcf9cc44dfb5f136810d80
Michael Färber <michael.faerber@gedenkt.at>
2021-07-08 10:52:17 +0200

    Silence warning.

--- dedukti-parse/src/lib.rs
+++ dedukti-parse/src/lib.rs
@@ -8,12 +8,14 @@ pub mod parse;
 pub use lex::Token;
 pub use parse::{Command, Error, Intro, Parse, Rule, Term};
 
-use alloc::vec::Vec;
 use logos::Logos;
 
 pub fn lex<'s>(s: &'s str) -> impl Iterator<Item = Token<'s>> {
     Token::lexer(s).filter(|token| *token != Token::Space)
 }
+
+#[cfg(feature = "itertools")]
+use alloc::vec::Vec;
 
 #[cfg(feature = "itertools")]
 pub fn lexes<'s>(s: &'s str) -> impl Iterator<Item = Result<Vec<Token<'s>>, Error>> {
