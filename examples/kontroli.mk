58ad10d57a0a91656deb13aa0760df1743ec57c5
Michael Färber <01mf02@gmail.com>
2022-02-10 12:11:26 +0100

    Allow customisation of the used kocheck version.

--- examples/kontroli.mk
+++ examples/kontroli.mk
@@ -1,5 +1,7 @@
+KOCHECK ?= kocheck
+
 %.dko:
 	@echo $<
 
 %.koo:
-	$(MAKE) --silent -f deps.mk -f kontroli.mk $*.dko | xargs cat | kocheck $(KOFLAGS) -
+	$(MAKE) --silent -f deps.mk -f kontroli.mk $*.dko | xargs cat | $(KOCHECK) $(KOFLAGS) -
