ff3861b7dee93d4ec5b9007cf1d1fcf80607556b
Michael Färber <01mf02@gmail.com>
2022-02-22 09:53:39 +0100

    Move around stuff.

--- kontroli/src/kernel/subst.rs
+++ kontroli/src/kernel/subst.rs
@@ -3,39 +3,6 @@
 use super::{RTerm, Term, TermC};
 use alloc::vec::Vec;
 
-impl<'s> RTerm<'s> {
-    pub fn apply_subst<S>(self, subst: &S, k: usize) -> Self
-    where
-        S: Fn(usize, usize) -> Term<'s>,
-    {
-        let sub = |tm: Term<'s>| tm.apply_subst(subst, k);
-        match &*self {
-            TermC::Appl(f, args) => {
-                let f2 = sub(f.clone());
-                let args2: Vec<Term> = args.iter().cloned().map(sub).collect();
-                if !f.ptr_eq(&f2) || !args.iter().zip(args2.iter()).all(|(a, a2)| a.ptr_eq(a2)) {
-                    return TermC::Appl(f2, args2).into();
-                }
-            }
-            TermC::Abst(arg, f) => {
-                let arg2 = arg.clone().map_type(|o| o.map(sub));
-                let f2 = f.clone().apply_subst(subst, k + 1);
-                if !arg.eq_ty(&arg2, Term::ptr_eq) || !f.ptr_eq(&f2) {
-                    return TermC::Abst(arg2, f2).into();
-                }
-            }
-            TermC::Prod(arg, f) => {
-                let arg2 = arg.clone().map_type(sub);
-                let f2 = f.clone().apply_subst(subst, k + 1);
-                if !arg.ty.ptr_eq(&arg2.ty) || !f.ptr_eq(&f2) {
-                    return TermC::Prod(arg2, f2).into();
-                }
-            }
-        };
-        self
-    }
-}
-
 impl<'s> Term<'s> {
     pub fn apply_subst<S>(self, subst: &S, k: usize) -> Self
     where
@@ -68,5 +35,38 @@ impl<'s> Term<'s> {
         } else {
             self.apply_subst(&|n, _k| Self::BVar(n + rhs), 0)
         }
+    }
+}
+
+impl<'s> RTerm<'s> {
+    pub fn apply_subst<S>(self, subst: &S, k: usize) -> Self
+    where
+        S: Fn(usize, usize) -> Term<'s>,
+    {
+        let sub = |tm: Term<'s>| tm.apply_subst(subst, k);
+        match &*self {
+            TermC::Appl(f, args) => {
+                let f2 = sub(f.clone());
+                let args2: Vec<Term> = args.iter().cloned().map(sub).collect();
+                if !f.ptr_eq(&f2) || !args.iter().zip(args2.iter()).all(|(a, a2)| a.ptr_eq(a2)) {
+                    return TermC::Appl(f2, args2).into();
+                }
+            }
+            TermC::Abst(arg, f) => {
+                let arg2 = arg.clone().map_type(|o| o.map(sub));
+                let f2 = f.clone().apply_subst(subst, k + 1);
+                if !arg.eq_ty(&arg2, Term::ptr_eq) || !f.ptr_eq(&f2) {
+                    return TermC::Abst(arg2, f2).into();
+                }
+            }
+            TermC::Prod(arg, f) => {
+                let arg2 = arg.clone().map_type(sub);
+                let f2 = f.clone().apply_subst(subst, k + 1);
+                if !arg.ty.ptr_eq(&arg2.ty) || !f.ptr_eq(&f2) {
+                    return TermC::Prod(arg2, f2).into();
+                }
+            }
+        };
+        self
     }
 }
