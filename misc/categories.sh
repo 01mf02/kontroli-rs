8c64407ddbfe1642a5ffc0aac6ce2946a138accf
Michael Färber <michael.faerber@gedenkt.at>
2022-03-09 19:27:42 +0100

    Update kernel statistics generator.

--- misc/categories.sh
+++ misc/categories.sh
@@ -1,19 +1,18 @@
 #!/bin/sh
 
 # Generate kernel size statistics to be used with pgf-pie.
+# Tested with tokei 12.1.2.
 # Call from main directory.
 
-cd src/kernel
+cd kontroli/src/kernel
 tokei -f * -o json | \
-  jq '.inner.Rust.stats | map({code, name})' | \
+  jq '.Total.children.Rust | map ({name, code: .stats.code})' | \
   sed \
-    -e "s/\(reduce\|matching\|state\).rs/Reduction/" \
-    -e "s/typing.rs/Typing/" \
+    -e "s/reduce.rs/Reduction/" \
+    -e "s/infer_check.rs/Typing/" \
     -e "s/subst.rs/Substitution/" \
-    -e "s/share.rs/Sharing/" \
-    -e "s/rterm.rs/Terms/" \
+    -e "s/sterm.rs/Terms/" \
     -e "s/convertible.rs/Convertibility/" \
     -e "s/mod.rs/API/" | \
-  jq 'group_by(.name) | map({name: .[0].name, code: map(.code) | add})' | \
   jq 'sort_by(.code) | reverse' | \
   jq -j '.[] | .code, "/", .name, ",\n"'
