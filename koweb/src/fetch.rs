b7a4976ad5ad2fd9632eb4d9af07526747c9127f
Michael Färber <01mf02@gmail.com>
2024-03-14 08:18:25 +0100

    Use web-sys instead of ureq to make requests.

--- koweb/src/fetch.rs
+++ koweb/src/fetch.rs
@@ -3,11 +3,31 @@ use error_chain::error_chain;
 error_chain! {
     foreign_links {
         Io(std::io::Error);
-        Ureq(ureq::Error);
     }
 }
 
-// TODO: remove async
 pub async fn fetch(url: &str) -> Result<String> {
-    Ok(ureq::get(url).call()?.into_string()?)
+    log::info!("Fetching {url} ...");
+    // TODO: handle errors
+    Ok(run(url).await.unwrap())
+}
+
+use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
+use wasm_bindgen_futures::JsFuture;
+use web_sys::Response;
+
+// Adapted from: https://rustwasm.github.io/wasm-bindgen/examples/fetch.html
+pub async fn run(url: &str) -> core::result::Result<String, JsValue> {
+    let window = web_sys::window().unwrap();
+    let resp_value = JsFuture::from(window.fetch_with_str(&url)).await?;
+
+    // `resp_value` is a `Response` object.
+    assert!(resp_value.is_instance_of::<Response>());
+    let resp: Response = resp_value.dyn_into().unwrap();
+
+    // Convert this other `Promise` into a rust `Future`.
+    let json = JsFuture::from(resp.text()?).await?;
+
+    Ok(json.as_string().unwrap())
 }
